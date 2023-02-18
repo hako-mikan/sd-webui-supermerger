cb6c1c50df34d77caa7eb63de08cf6c7cd07f571
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2023-02-18 23:10:12 +0900

    Update supermerger.py

--- scripts/supermerger.py
+++ scripts/supermerger.py
@@ -336,6 +336,8 @@ mlist=[]
 def load_historyf():
     filepath = os.path.join(path_root,"mergehistory.csv")
     global mlist,msearch
+    msearch = []
+    mlist=[]
     try:
         with  open(filepath, 'r') as f:
             reader = csv.reader(f)
@@ -361,6 +363,7 @@ def searchhistory(words,searchmode):
                 if w not in m:hit = False
             else:
                 if w in m:hit = True
+        print(i,len(mlist))
         if hit :outs.append(mlist[i])
 
     if outs == []:return [["no result","",""],]
