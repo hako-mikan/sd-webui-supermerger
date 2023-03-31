c220b0c5ff118bdaf2977eeb6e38c4a31c158514
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2023-04-01 00:49:40 +0900

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import launch
 
-if not launch.is_installed("scikit-learn"):
+if not launch.is_installed("scikit"):
     launch.run_pip("install scikit-learn", "scikit-learn")
 
 if not launch.is_installed("diffusers"):
