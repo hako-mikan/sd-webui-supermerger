b69a3d27e097e38bc2c064e27020eca59b2ad744
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2023-07-02 00:46:13 +0900

    bugfixfix

--- scripts/mergers/mergers.py
+++ scripts/mergers/mergers.py
@@ -841,12 +841,12 @@ def blocker(blocks):
         changer = True
         if "-" in w:
             wt = [wt.strip() for wt in w.split('-')]
-            if  BLOCKID.index(wt[1]) > BLOCKID.index(wt[0]):
-                flagger[BLOCKID.index(wt[0]):BLOCKID.index(wt[1])+1] = [changer]*(BLOCKID.index(wt[1])-BLOCKID.index(wt[0])+1)
+            if  blockid.index(wt[1]) > blockid.index(wt[0]):
+                flagger[blockid.index(wt[0]):blockid.index(wt[1])+1] = [changer]*(blockid.index(wt[1])-blockid.index(wt[0])+1)
             else:
-                flagger[BLOCKID.index(wt[1]):BLOCKID.index(wt[0])+1] = [changer]*(BLOCKID.index(wt[0])-BLOCKID.index(wt[1])+1)
+                flagger[blockid.index(wt[1]):blockid.index(wt[0])+1] = [changer]*(blockid.index(wt[0])-blockid.index(wt[1])+1)
         else:
             output = output + " " + w if output else w
         for i in range(26):
-            if flagger[i]: output = output + " " + BLOCKID[i] if output else BLOCKID[i]
+            if flagger[i]: output = output + " " + blockid[i] if output else blockid[i]
     return output
