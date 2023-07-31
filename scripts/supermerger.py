c1f2e88db8dd4b3c1e5ebfe224dbf29c3253dcea
File_xor <filexor@gmail.com>
2023-08-01 00:46:51 +0900

    Add GenParamGetter

--- scripts/supermerger.py
+++ scripts/supermerger.py
@@ -27,6 +27,60 @@ from scripts.mergers.mergers import (TYPESEG, freezemtime, rwmergelog, simggen,s
 from scripts.mergers.xyplot import freezetime, nulister, numanager
 from scripts.mergers.model_util import filenamecutter
 
+class GenParamGetter(scripts.Script):
+    txt2img_gen_button = None
+    img2img_gen_button = None
+
+    txt2img_args = None
+    img2img_args = None
+
+    def after_component(self, component: gr.components.Component, **_kwargs):
+        """Find generate button"""
+        if component.elem_id == "txt2img_generate":
+            self.txt2img_gen_button = component
+        elif  component.elem_id == "img2img_generate":
+            self.img2img_gen_button = component
+
+    def get_components_by_ids(self, root: gr.Blocks, ids: list[int]):
+        components: list[gr.Blocks] = []
+
+        if root._id in ids:
+            components.append(root)
+            ids = [_id for _id in ids if _id != root._id]
+
+        if isinstance(root, gr.BlockContext):
+            for block in root.children:
+                components.extend(self.get_components_by_ids(block, ids))
+
+        return components
+    
+    def compare_components_with_ids(components: list[gr.Blocks], ids: list[int]):
+        return len(components) == len(ids) and all(component._id == _id for component, _id in zip(components, ids))
+
+    def get_params_components(self, demo: gr.Blocks, app):
+        dependencies: list[dict] = [x for x in demo.dependencies if x["trigger"] == "click" and (self.txt2img_gen_button._id if self.is_txt2img else self.img2img_gen_button) in x["targets"]]
+        dependency: dict = None
+        cnet_dependency: dict = None
+        UiControlNetUnit = None
+        for d in dependencies:
+            if len(d["outputs"]) == 1:
+                outputs = outputs = self.get_components_by_ids(demo, d["outputs"])
+                output = outputs[0]
+                if (
+                    isinstance(output, gr.State)
+                    and type(output.value).__name__ == "UiControlNetUnit"
+                ):
+                    cnet_dependency = d
+                    UiControlNetUnit = type(output.value)
+
+            elif len(d["outputs"]) == 4:
+                dependency = d
+
+        if self.is_txt2img:
+            self.txt2img_args = next(args for args in demo.fns if self.compare_components_with_ids(demo.inputs, dependency["inputs"]))
+        elif self.is_img2img:
+            self.img2img_args = next(args for args in demo.fns if self.compare_components_with_ids(demo.inputs, dependency["inputs"]))
+
 gensets=argparse.Namespace()
 
 def on_ui_train_tabs(params):
