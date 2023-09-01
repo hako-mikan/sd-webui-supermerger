123945e26b0871435c0736aad60231fe20461c7a
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2023-09-01 21:57:23 +0900

    fix for override settings

--- scripts/mergers/mergers.py
+++ scripts/mergers/mergers.py
@@ -990,7 +990,7 @@ def simggen(s_prompt,s_nprompt,s_steps,s_sampler,s_cfg,s_seed,s_w,s_h,s_batch_si
         hr_second_pass_steps=g("Hires steps","Secondary steps"),
         hr_resize_x=g("Resize width to"),
         hr_resize_y=g("Resize height to"),
-        override_settings=g("Override settings"),
+        override_settings=create_override_settings_dict(g("Override settings")),
         do_not_save_grid=True,
         do_not_save_samples=True,
         do_not_reload_embeddings=True,
