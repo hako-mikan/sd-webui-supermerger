hako-mikan/sd-webui-supermerger

commit 33b8ddd4c9d6ee94f7f41df6acdf79639275ea80

support Save current merged model

scripts/mergers/model_util.py
@@ -673,10 +673,44 @@ def to_half(sd):
             sd[key] = sd[key].half()
     return sd
 
-def savemodel(state_dict,currentmodel,fname,savesets,model_a,metadata={}):
+def savemodel(state_dict,currentmodel,fname,savesets,metadata={}):
+    import json
     from modules import sd_models,shared
+
+    if state_dict is None:
+        if shared.sd_model and shared.sd_model.sd_checkpoint_info:
+            metadata = shared.sd_model.sd_checkpoint_info.metadata.copy()
+        else:
+            return "Current model is not a valid merged model"
+
+        checkpoint_info = shared.sd_model.sd_checkpoint_info
+        # check if current merged model is a fake checkpoint_info
+        if checkpoint_info is not None:
+            filename = checkpoint_info.filename
+            name = os.path.basename(filename)
+            info = sd_models.get_closet_checkpoint_match(name)
+            if info == checkpoint_info:
+                # this is a valid checkpoint_info
+                # no need to save
+                return "Current model is not a merged model or you've already saved model"
+
+        # prepare metadata
+        save_metadata = "save metadata" in savesets
+        if save_metadata:
+            metadata["sd_merge_models"] = json.dumps(metadata["sd_merge_models"])
+        else:
+            metadata = {"format": "pt"}
+
+        if shared.sd_model is not None:
+            print("load from shared.sd_model..")
+            state_dict = shared.sd_model.state_dict()
+        else:
+            return "No current loaded model found"
+
+        # name_for_extra was set with the currentmodel
+        currentmodel = checkpoint_info.name_for_extra
+
     if "fp16" in savesets: 
-        state_dict = to_half(state_dict)
         pre = ".fp16"
     else:pre = ""
     ext = ".safetensors" if "safetensors" in savesets else ".ckpt"
@@ -689,17 +723,13 @@ def savemodel(state_dict,currentmodel,fname,savesets,model_a,metadata={}):
         if shape[1] == 8:
             pre += "-instruct-pix2pix"
 
-    checkpoint_info = sd_models.get_closet_checkpoint_match(model_a)
-    model_a_path= checkpoint_info.filename
-    modeldir = os.path.split(model_a_path)[0]
-
     if not fname or fname == "":
         fname = currentmodel.replace(" ","").replace(",","_").replace("(","_").replace(")","_")+pre+ext
         if fname[0]=="_":fname = fname[1:]
     else:
         fname = fname if ext in fname else fname +pre+ext
 
-    fname = os.path.join(modeldir, fname)
+    fname = os.path.join(sd_models.model_path, fname)
     fname = fname.replace("ProgramFiles_x86_","Program Files (x86)")
 
     if len(fname) > 255:
@@ -713,6 +743,16 @@ def savemodel(state_dict,currentmodel,fname,savesets,model_a,metadata={}):
         return _err_msg
 
     print("Saving...")
+    isxl = "conditioner.embedders.1.model.transformer.resblocks.9.mlp.c_proj.weight" in state_dict
+    if isxl:
+        # prune share memory tensors, "cond_stage_model." prefixed base tensors are share memory with "conditioner." prefixed tensors
+        for i, key in enumerage(state_dict.keys()):
+            if "cond_stage_model." in key:
+                del state_dict[key]
+
+    if "fp16" in savesets:
+        state_dict = to_half(state_dict)
+
     try:
       if ext == ".safetensors":
           safetensors.torch.save_file(state_dict, fname, metadata=metadata)

scripts/mergers/mergers.py
@@ -134,7 +134,7 @@ def smergegen(weights_a,weights_b,model_a,model_b,model_c,base_alpha,base_beta,m
 
     save = True if SAVEMODES[0] in save_sets else False
 
-    result = savemodel(theta_0,currentmodel,custom_name,save_sets,model_a,metadata) if save else "Merged model loaded:"+currentmodel
+    result = savemodel(theta_0,currentmodel,custom_name,save_sets,metadata) if save else "Merged model loaded:"+currentmodel
 
     sd_models.model_data.__init__()
     load_model(checkpoint_info, already_loaded_state_dict=theta_0)
@@ -168,6 +168,7 @@ def fake_checkpoint_info(checkpoint_info,metadata,currentmodel):
     checkpoint_info.name = checkpoint_info.name_for_extra + ".safetensors"
     checkpoint_info.model_name = checkpoint_info.name_for_extra.replace("/", "_").replace("\\", "_")
     checkpoint_info.title = f"{checkpoint_info.name} [{sha256[0:10]}]"
+    checkpoint_info.metadata = metadata
 
         # force to set a new sha256 hash
     if c_cache is not None: 
@@ -233,7 +234,6 @@ def smerge(weights_a,weights_b,model_a,model_b,model_c,base_alpha,base_beta,mode
     # mode select booleans
     save = True if SAVEMODES[0] in save_sets else False
     usebeta = MODES[2] in mode or MODES[3] in mode or "tensor" in calcmode
-    save_metadata = "save metadata" in save_sets
     metadata = {"format": "pt"}
 
     if not useblocks:
@@ -696,7 +696,7 @@ def smerge(weights_a,weights_b,model_a,model_b,model_c,base_alpha,base_beta,mode
 
     caster(mergedmodel,False)
 
-    if save_metadata:
+    if True: # always set metadata. savemodel() will check save_sets later
         merge_recipe = {
             "type": "sd-webui-supermerger",
             "weights_alpha": weights_a if useblocks else None,

scripts/mergers/pluslora.py
@@ -629,7 +629,7 @@ def pluslora(lnames,loraratios,settings,output,model,precision):
     #usemodelgen(theta_0,model)
     settings.append(precision)
     settings.append("safetensors")
-    result = savemodel(theta_0,dname,output,settings,model)
+    result = savemodel(theta_0,dname,output,settings)
     del theta_0
     gc.collect()
     return result

scripts/mergers/xyplot.py
@@ -407,7 +407,7 @@ def sgenxyplot(xtype,xmen,ytype,ymen,ztype,zmen,esettings,
                     load_model(checkpoint_info, already_loaded_state_dict=theta_0)
 
                 if "save model" in esettings:
-                    savemodel(theta_0,currentmodel,custom_name,save_sets,model_a,metadata) 
+                    savemodel(theta_0,currentmodel,custom_name,save_sets,metadata) 
                 theta_0 = None
                 del theta_0
 

scripts/supermerger.py
@@ -26,7 +26,7 @@ import csv
 import scripts.mergers.pluslora as pluslora
 from scripts.mergers.mergers import (TYPESEG, freezemtime, rwmergelog, blockfromkey, clearcache, getcachelist)
 from scripts.mergers.xyplot import freezetime, nulister
-from scripts.mergers.model_util import filenamecutter
+from scripts.mergers.model_util import filenamecutter, savemodel
 
 path_root = basedir()
 
@@ -99,6 +99,9 @@ def on_ui_tabs():
                                 with gr.Row():
                                     bake_in_vae = gr.Dropdown(choices=["None"] + list(sd_vae.vae_dict), value="None", label="Bake in VAE", elem_id="modelmerger_bake_in_vae")
                                     create_refresh_button(bake_in_vae, sd_vae.refresh_vae_list, lambda: {"choices": ["None"] + list(sd_vae.vae_dict)}, "modelmerger_refresh_bake_in_vae")
+
+                        with gr.Row():
+                            savecurrent = gr.Button(elem_id="savecurrent", elem_classes=["compact_button"], value="Save current merge")
 
                     with gr.Row():
                         components.merge = gr.Button(elem_id="model_merger_merge", elem_classes=["compact_button"], value="Merge!",variant='primary')
@@ -449,6 +452,10 @@ def on_ui_tabs():
         mode.change(fn=lambda mode: [gr.update(info=mode_info[mode]), gr.update(interactive=True if mode in ["Triple sum", "sum Twice"] else False)], inputs=[mode], outputs=[mode, base_beta], show_progress=False)
         useblocks.change(fn=lambda mbw: gr.update(visible=False if mbw else True), inputs=[useblocks], outputs=[alpha_group])
 
+        def save_current_merge(custom_name, save_settings):
+            msg = savemodel(None,None,custom_name,save_settings)
+            return gr.update(value=msg)
+
         def addblockweights(val, blockopt, *blocks):
             if val == "none":
                 val = 0
@@ -550,6 +557,8 @@ def on_ui_tabs():
                 value = float(opt)
 
             return gr.update(value = value)
+
+        savecurrent.click(fn=save_current_merge, inputs=[custom_name, save_sets], outputs=[components.submit_result])
 
         resetopt.change(fn=resetvalopt,inputs=[resetopt],outputs=[resetval])
         resetweight.click(fn=resetblockweights,inputs=[resetval,resetblockopt],outputs=menbers)

commit bd5d7a19dbd0840557ccbe8ba50e9327a27afde5

typo regression by commit 33b8ddd

scripts/mergers/model_util.py
@@ -754,7 +754,7 @@ def savemodel(state_dict,currentmodel,fname,savesets,metadata={}):
     isxl = "conditioner.embedders.1.model.transformer.resblocks.9.mlp.c_proj.weight" in state_dict
     if isxl:
         # prune share memory tensors, "cond_stage_model." prefixed base tensors are share memory with "conditioner." prefixed tensors
-        for i, key in enumerage(state_dict.keys()):
+        for i, key in enumerate(state_dict.keys()):
             if "cond_stage_model." in key:
                 del state_dict[key]
 